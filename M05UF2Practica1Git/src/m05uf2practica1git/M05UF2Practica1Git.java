dcbd7c3333351e511dc066daa91a7a0c3240e90e
Gerard Fernandez <gfernandez@ieslaferreria.net>
2024-03-11 20:27:58 +0100

    Error de potencia

--- M05UF2Practica1Git/src/m05uf2practica1git/M05UF2Practica1Git.java
+++ M05UF2Practica1Git/src/m05uf2practica1git/M05UF2Practica1Git.java
@@ -81,7 +81,7 @@ public class M05UF2Practica1Git {
                         res = n1 / n2;
                         break;
                     case "*":
-                        res = Math.pow(n1, n1);
+                        res = Math.pow(n1, n2);
                         break;
                     case "%":
                         while (n2 == 0) {
